0fd68a933c937bc72c6b9a0976f1479535ebf673
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-04-24 13:38:56 -0500

    Add while(true) loop

--- InclusiveCodeReviews/InclusiveCodeReviews.ConsoleApp/Program.cs
+++ InclusiveCodeReviews/InclusiveCodeReviews.ConsoleApp/Program.cs
@@ -13,20 +13,24 @@ namespace InclusiveCodeReviews.ConsoleApp
     {
         static void Main(string[] args)
         {
-            // Create single instance of sample data from first line of dataset for model input
-            ModelInput sampleData = new ModelInput()
+            while (true)
             {
-                Text = @"-   (Er...let's shimmy)  08:43, 17 Apr 2005 (UTC)",
-            };
+                Console.WriteLine("Enter some text:");
 
-            // Make a single prediction on the sample data and print results
-            var predictionResult = ConsumeModel.Predict(sampleData);
+                // Create single instance of sample data from first line of dataset for model input
+                ModelInput sampleData = new ModelInput()
+                {
+                    Text = Console.ReadLine(),
+                };
 
-            Console.WriteLine("Using model to make single prediction -- Comparing actual Istoxic with predicted Istoxic from sample data...\n\n");
-            Console.WriteLine($"Text: {sampleData.Text}");
-            Console.WriteLine($"\n\nPredicted Istoxic value {predictionResult.Prediction} \nPredicted Istoxic scores: [{String.Join(",", predictionResult.Score)}]\n\n");
-            Console.WriteLine("=============== End of process, hit any key to finish ===============");
-            Console.ReadKey();
+                // Make a single prediction on the sample data and print results
+                var predictionResult = ConsumeModel.Predict(sampleData);
+
+                Console.WriteLine("Using model to make single prediction -- Comparing actual Istoxic with predicted Istoxic from sample data...\n\n");
+                Console.WriteLine($"Text: {sampleData.Text}");
+                Console.WriteLine($"\n\nPredicted Istoxic value {predictionResult.Prediction} \nPredicted Istoxic scores: [{String.Join(",", predictionResult.Score)}]\n\n");
+                Console.WriteLine("=============== End of process, hit any key to finish ===============");
+            }
         }
     }
 }
